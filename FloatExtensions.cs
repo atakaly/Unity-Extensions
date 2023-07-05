eb463d77d0d578af299782481a246637469d3f90
atakaly <79188326+atakaly@users.noreply.github.com>
2023-07-05 12:33:48 +0300

    Remap function's parameter order changed

--- FloatExtensions.cs
+++ FloatExtensions.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class FloatExtensions
 {
-    public static float Remap(this float value, float from1, float to1, float from2, float to2)
+    public static float Remap(this float value, float from1, float from2, float to1, float to2)
     {
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
